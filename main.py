e58f07714d3c3bd94f4319d2c4c51cdd78f2c237
Mayuri Pratapsi <mayuridsp@gmail.com>
2022-08-21 12:47:12 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+
+player = Player()
+screen.listen()
+screen.onkey(player.move, "Up")
+score = Scoreboard()
+
+car_manager = CarManager()
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car and eng game
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            score.game_over()
+
+    # Increase score
+    if player.ycor() > 280:
+        player.restart()
+        score.increase_score()
+        car_manager.level_up()
+
+
+screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.hideturtle()
+        self.goto(-200, 260)
+        self.score = 0
+        self.update_score()
+
+    def update_score(self):
+        self.clear()
+        self.write(f"Level: {self.score}", align="center", font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
